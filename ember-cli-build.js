d51aadbae7e0a34289036e2c3c677aca7c6bd840
vinod <vinod@talie4.com>
2021-11-27 13:33:17 +0530

    Initial Commit

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,8 @@ module.exports = function (defaults) {
     // Add options here
   });
 
+  app.import('node_modules/todomvc-common/base.css');
+  app.import('node_modules/todomvc-app-css/index.css');
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
